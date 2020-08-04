3263fa5c408764c7a4b4612fdaa7b4de4826c70d
Valeria Chiran <vmchiran@users.noreply.github.com>
2020-08-04 10:03:37 +0300

    updates

--- tf/soacs-soa-psm-priv-prereq/main.tf
+++ tf/soacs-soa-psm-priv-prereq/main.tf
@@ -2,7 +2,7 @@
 resource "oci_identity_compartment" "soacs" {
   #Required
   name           = "${var.prefix}-${var.compartment_name}"
-  description    = "SOACS compartment created by Terraform"
+  description    = "SOA PSM with 2-node cluster, Private Network and Public LB"
   #Parent compartment id
   compartment_id = var.compartment_ocid
 
@@ -27,7 +27,7 @@ resource "oci_core_internet_gateway" "ig" {
   vcn_id         = oci_core_vcn.vcn.id
 
   #Optional
-  display_name = "soa-ig1"
+  display_name = "${var.prefix}-ig"
 }
 
 # NAT gateway is required for the private node of the SOACS instance to access the public Internet
@@ -37,7 +37,7 @@ resource "oci_core_nat_gateway" "ng" {
   vcn_id         = oci_core_vcn.vcn.id
 
   #Optional
-  display_name = "soa-ng1"
+  display_name = "${var.prefix}-ng"
 }
 
 # The service gateway is required for the private node of the SOACS instance to access the Oracle Services, such as Object Storage
@@ -51,10 +51,10 @@ resource "oci_core_service_gateway" "sg" {
   }
 
   #Optional
-  display_name   = "soa-sg1"
+  display_name   = "${var.prefix}-sg"
 }
 
-resource "oci_core_route_table" "soa_public_route_table" {
+resource "oci_core_route_table" "public_route_table" {
   compartment_id = oci_identity_compartment.soacs.id
   vcn_id         = oci_core_vcn.vcn.id
 
@@ -69,10 +69,10 @@ resource "oci_core_route_table" "soa_public_route_table" {
   }
 
   #Optional
-  display_name = "soa-public-rt1"
+  display_name = "${var.prefix}-public-rt"
 }
 
-resource "oci_core_route_table" "soa_private_route_table" {
+resource "oci_core_route_table" "private_route_table" {
   compartment_id = oci_identity_compartment.soacs.id
   vcn_id         = oci_core_vcn.vcn.id
 
@@ -97,21 +97,161 @@ resource "oci_core_route_table" "soa_private_route_table" {
   }
 
   #Optional
-  display_name = "soa-private-rt1"
+  display_name = "${var.prefix}-private-rt"
 }
 
 
 # Protocols are specified as protocol numbers.
 # http://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
 
-# Security List for the Bastion Subnet
-resource "oci_core_security_list" "soa_bastion_security_list" {
+# Security List for the Load Balancer Subnet
+resource "oci_core_security_list" "lb_security_list" {
   #Required
   compartment_id = oci_identity_compartment.soacs.id
   vcn_id         = oci_core_vcn.vcn.id
 
   #Optional
-  display_name   = "soa-bastion-sl1"
+  display_name   = "${var.prefix}-lb-sl"
+
+  // allow outbound tcp traffic on all ports
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "all"
+  }
+
+  // allow inbound tcp traffic for HTTP 80
+  ingress_security_rules {
+    #Required
+    protocol  = "6"   // tcp
+    source    = var.vcn_cidr
+
+    #Optional
+    stateless = false
+
+    tcp_options {
+      // These values correspond to the destination port range.
+      min = 80
+      max = 80
+    }
+  }
+
+  // allow inbound tcp traffic for HTTPS 443
+  ingress_security_rules {
+    #Required
+    protocol  = "6"   // tcp
+    source    = var.vcn_cidr
+
+    #Optional
+    stateless = false
+
+    tcp_options {
+      // These values correspond to the destination port range.
+      min = 443
+      max = 443
+    }
+  }
+}
+
+resource "oci_core_security_list" "wls_lb_security_list" {
+  #Required
+  compartment_id = oci_identity_compartment.soacs.id
+  vcn_id         = oci_core_vcn.vcn.id
+
+  #Optional
+  display_name   = "${var.prefix}-wls-lb-sl"
+
+  // allow inbound tcp traffic on 9073, for the wls managed
+  ingress_security_rules {
+    #Required
+    protocol  = "6"   // tcp
+    source    = var.subnet_lb_private_cidr
+
+    #Optional
+    stateless = false
+
+    tcp_options {
+      // These values correspond to the destination port range.
+      min = 9073
+      max = 9073
+    }
+  }
+}
+
+resource "oci_core_security_list" "internal_security_list" {
+  #Required
+  compartment_id = oci_identity_compartment.soacs.id
+  vcn_id         = oci_core_vcn.vcn.id
+
+  #Optional
+  display_name   = "${var.prefix}-internal-sl"
+
+
+  // allow inbound tcp traffic for the subnet
+  ingress_security_rules {
+    #Required
+    protocol  = "6"   // tcp
+    source    = var.subnet_private_cidr
+
+    #Optional
+    stateless = false
+  }
+}
+
+resource "oci_core_security_list" "wls_security_list" {
+  #Required
+  compartment_id = oci_identity_compartment.soacs.id
+  vcn_id         = oci_core_vcn.vcn.id
+
+  #Optional
+  display_name   = "${var.prefix}-wls-sl"
+
+  // allow outbound tcp traffic on all ports
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "all"
+  }
+
+  // allow inbound ssh traffic
+  ingress_security_rules {
+    #Required
+    protocol  = "6"   // tcp
+    source    = var.subnet_bastion_cidr
+
+    #Optional
+    stateless = false
+
+    tcp_options {
+      // These values correspond to the destination port range.
+      min = 22
+      max = 22
+    }
+  }
+
+  // allow inbound tcp traffic on 7002, for wls admin
+  ingress_security_rules {
+    #Required
+    protocol  = "6"   // tcp
+    source    = var.vcn_cidr
+
+    #Optional
+    stateless = false
+
+    tcp_options {
+      // These values correspond to the destination port range.
+      min = 7002
+      max = 7002
+    }
+  }
+}
+
+# Security List for the Bastion Subnet
+resource "oci_core_security_list" "bastion_security_list" {
+  #Required
+  compartment_id = oci_identity_compartment.soacs.id
+  vcn_id         = oci_core_vcn.vcn.id
+
+  #Optional
+  display_name   = "${var.prefix}-bastion-sl"
 
   // allow outbound tcp traffic on all ports
   egress_security_rules {
@@ -136,77 +276,6 @@ resource "oci_core_security_list" "soa_bastion_security_list" {
   }
 }
 
-# Security List for the Load Balancer Subnets
-resource "oci_core_security_list" "soa_lb_security_list" {
-  #Required
-  compartment_id = oci_identity_compartment.soacs.id
-  vcn_id         = oci_core_vcn.vcn.id
-
-  #Optional
-  display_name   = "soa-lb-sl1"
-
-  // allow outbound traffic
-  egress_security_rules {
-    destination = "0.0.0.0/0"
-    protocol    = "all"
-  }
-
-  // allow inbound tcp traffic for HTTP 80
-  ingress_security_rules {
-    #Required
-    protocol  = "6"   // tcp
-    source    = "0.0.0.0/0"
-
-    #Optional
-    stateless = false
-
-    tcp_options {
-      // These values correspond to the destination port range.
-      min = 80
-      max = 80
-    }
-  }
-
-  // allow inbound tcp traffic for HTTPS 443
-  ingress_security_rules {
-    #Required
-    protocol  = "6"   // tcp
-    source    = "0.0.0.0/0"
-
-    #Optional
-    stateless = false
-
-    tcp_options {
-      // These values correspond to the destination port range.
-      min = 443
-      max = 443
-    }
-  }
-}
-
-# Security List for the Private Subnet
-resource "oci_core_security_list" "soa_private_security_list" {
-  #Required
-  compartment_id = oci_identity_compartment.soacs.id
-  vcn_id         = oci_core_vcn.vcn.id
-
-  #Optional
-  display_name   = "soa-private-sl1"
-
-  // allow outbound tcp traffic on all ports
-  egress_security_rules {
-    destination = "0.0.0.0/0"
-    protocol    = "all"
-  }
-
-  // allow inbound ssh traffic
-  ingress_security_rules {
-    #Required
-    protocol  = "all"
-    source    = var.vcn_cidr
-  }
-}
-
 resource "oci_core_subnet" "soa_bastion_subnet" {
   #Required
   cidr_block        = var.subnet_bastion_cidr
@@ -217,9 +286,9 @@ resource "oci_core_subnet" "soa_bastion_subnet" {
   display_name    = "soa-bastion-subnet1"
   dhcp_options_id = oci_core_vcn.vcn.default_dhcp_options_id
   dns_label       = "soabastion1"
-  route_table_id  = oci_core_route_table.soa_public_route_table.id
+  route_table_id  = oci_core_route_table.public_route_table.id
   # security_list_ids = [oci_core_vcn.vcn.default_security_list_id, oci_core_security_list.security_list.id]
-  security_list_ids = [oci_core_security_list.soa_bastion_security_list.id]
+  security_list_ids = [oci_core_security_list.bastion_security_list.id]
 }
 
 resource "oci_core_subnet" "soa_lb_private_subnet" {
@@ -233,9 +302,9 @@ resource "oci_core_subnet" "soa_lb_private_subnet" {
   dhcp_options_id = oci_core_vcn.vcn.default_dhcp_options_id
   dns_label       = "soalbprivate1"
   prohibit_public_ip_on_vnic = true
-  route_table_id  = oci_core_route_table.soa_private_route_table.id
+  route_table_id  = oci_core_route_table.private_route_table.id
   # security_list_ids = [oci_core_vcn.vcn.default_security_list_id, oci_core_security_list.security_list.id]
-  security_list_ids = [oci_core_security_list.soa_lb_security_list.id]
+  security_list_ids = [oci_core_security_list.lb_security_list.id]
 }
 
 resource "oci_core_subnet" "soa_private_subnet" {
@@ -249,9 +318,9 @@ resource "oci_core_subnet" "soa_private_subnet" {
   dhcp_options_id = oci_core_vcn.vcn.default_dhcp_options_id
   dns_label       = "soaprivate1"
   prohibit_public_ip_on_vnic = true
-  route_table_id  = oci_core_route_table.soa_private_route_table.id
+  route_table_id  = oci_core_route_table.private_route_table.id
   # security_list_ids = [oci_core_vcn.vcn.default_security_list_id, oci_core_security_list.security_list.id]
-  security_list_ids = [oci_core_security_list.soa_private_security_list.id]
+  security_list_ids = [oci_core_security_list.wls_lb_security_list.id, oci_core_security_list.internal_security_list.id, oci_core_security_list.wls_security_list.id]
 }
 
 # Define the Security Policy
@@ -259,7 +328,7 @@ resource "oci_identity_policy" "soa_policy" {
   #Required
   compartment_id = oci_identity_compartment.soacs.id
   description    = "SOACS PSM Policy created by Terraform"
-  name           = "soa_policy"
+  name           = "soa-psm-policy"
 
   statements = [
     "Allow service PSM to inspect vcns in compartment ${oci_identity_compartment.soacs.name}",
@@ -282,81 +351,82 @@ resource "oci_objectstorage_bucket" "soa_bucket" {
   access_type = "NoPublicAccess"
 }
 
-# # Create the Bastion
-# resource "oci_core_instance" "bastion" {
-#   # Required
-#   availability_domain = data.oci_identity_availability_domain.ad.name
-#   compartment_id      = oci_identity_compartment.soacs.id
-#   shape               = var.bastion_shape
+# Create the DBSystem
+resource "oci_database_db_system" "soa_db_system" {
+  #Required
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = oci_identity_compartment.soacs.id
+  database_edition    = var.db_system_edition
 
-#   # display_name = "vch-build-server"
-#   display_name = "${var.prefix}-${var.bastion_name}"
+  db_home {
+    #Required
+    database {
+      #Required
+      admin_password = var.db_admin_password
 
-#   # Optional
-#   create_vnic_details {
-#     # Required
-#     subnet_id = oci_core_subnet.soa_bastion_subnet.id
+      #Optional
+      db_backup_config {
+        auto_backup_enabled = false
+      }
 
-#     # Optional
-#     assign_public_ip = true
-#   }
+      db_name = var.db_name
+    }
 
-#   source_details {
-#     # Required
-#     source_type = "image"
-#     source_id   = var.imageocids[var.region]
+    #Optional
+    db_version = var.db_version
+  }
 
-#     # Apply this to set the size of the boot volume that's created for this instance.
-#     # Otherwise, the default boot volume size of the image is used.
-#     # This should only be specified when source_type is set to "image".
-#     #boot_volume_size_in_gbs = "60"
-#   }
+  hostname        = var.db_hostname
+  shape           = var.db_system_shape
+  ssh_public_keys = [file(var.ssh_public_key_path)]
+  subnet_id       = oci_core_subnet.soa_private_subnet.id
 
-#   # Apply the following flag only if you wish to preserve the attached boot volume upon destroying this instance
-#   # Setting this and destroying the instance will result in a boot volume that should be managed outside of this config.
-#   # When changing this value, make sure to run 'terraform apply' so that it takes effect before the resource is destroyed.
-#   #preserve_boot_volume = true
+  #Optional
+  display_name            = "${var.prefix}-${var.soa_db_system_display_name}"
+  #Initial data storage size (in GB) must be one of [256, 512, 1024, 2048, 4096, 6144, 8192, 10240, 12288, 14336, 16384, 18432, 20480, 22528, 24576, 26624, 28672, 30720, 32768, 34816, 36864, 38912, 40960]
+  data_storage_size_in_gb = var.data_storage_size_in_gb
+  license_model           = var.license_model
+  # node_count              = data.oci_database_db_system_shapes.soa_db_system_shapes.db_system_shapes[0]["minimum_node_count"]
+  node_count              = 2
+}
 
-#   metadata = {
-#     ssh_authorized_keys = file(var.ssh_public_key_path)
-#     # user_data           = base64encode(file(var.BootStrapFile))
-#   }
-# }
+# Create the Bastion
+resource "oci_core_instance" "bastion" {
+  # Required
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = oci_identity_compartment.soacs.id
+  shape               = var.bastion_shape
 
-# # Create the DBSystem
-# resource "oci_database_db_system" "soa_db_system" {
-#   #Required
-#   availability_domain = data.oci_identity_availability_domain.ad.name
-#   compartment_id      = oci_identity_compartment.soacs.id
-#   database_edition    = var.db_system_edition
+  # display_name = "vch-build-server"
+  display_name = "${var.prefix}-${var.bastion_name}"
 
-#   db_home {
-#     #Required
-#     database {
-#       #Required
-#       admin_password = var.db_admin_password
+  # Optional
+  create_vnic_details {
+    # Required
+    subnet_id = oci_core_subnet.soa_bastion_subnet.id
 
-#       #Optional
-#       db_backup_config {
-#         auto_backup_enabled = false
-#       }
+    # Optional
+    assign_public_ip = true
+  }
 
-#       db_name = var.soa_db1_name
-#     }
+  source_details {
+    # Required
+    source_type = "image"
+    source_id   = var.imageocids[var.region]
 
-#     #Optional
-#     db_version = var.db_version
-#   }
+    # Apply this to set the size of the boot volume that's created for this instance.
+    # Otherwise, the default boot volume size of the image is used.
+    # This should only be specified when source_type is set to "image".
+    #boot_volume_size_in_gbs = "60"
+  }
 
-#   hostname        = var.soa_db_hostname
-#   shape           = var.db_system_shape
-#   ssh_public_keys = [file(var.ssh_public_key_path)]
-#   subnet_id       = oci_core_subnet.soa_private_subnet.id
+  # Apply the following flag only if you wish to preserve the attached boot volume upon destroying this instance
+  # Setting this and destroying the instance will result in a boot volume that should be managed outside of this config.
+  # When changing this value, make sure to run 'terraform apply' so that it takes effect before the resource is destroyed.
+  #preserve_boot_volume = true
 
-#   #Optional
-#   display_name            = "${var.prefix}-${var.soa_db_system_display_name}"
-#   #Initial data storage size (in GB) must be one of [256, 512, 1024, 2048, 4096, 6144, 8192, 10240, 12288, 14336, 16384, 18432, 20480, 22528, 24576, 26624, 28672, 30720, 32768, 34816, 36864, 38912, 40960]
-#   data_storage_size_in_gb = var.data_storage_size_in_gb
-#   license_model           = var.license_model
-#   node_count              = data.oci_database_db_system_shapes.soa_db_system_shapes.db_system_shapes[0]["minimum_node_count"]
-# }
+  metadata = {
+    ssh_authorized_keys = file(var.ssh_public_key_path)
+    # user_data           = base64encode(file(var.BootStrapFile))
+  }
+}
--- tf/soacs-soa-psm-priv-prereq/variables.tf
+++ tf/soacs-soa-psm-priv-prereq/variables.tf
@@ -62,7 +62,13 @@ variable "vcn_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "subnet_public_cidr" {
+variable "subnet_private_cidr" {
+  type        = string
+  description = "cidr block of subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_lb_private_cidr" {
   type        = string
   description = "cidr block of subnet"
   default     = "10.0.1.0/24"
@@ -74,23 +80,15 @@ variable "subnet_bastion_cidr" {
   default     = "10.0.1.0/24"
 }
 
-variable "subnet_private_cidr" {
-  type        = string
-  description = "cidr block of subnet"
-  default     = "10.0.2.0/24"
-}
-
-variable "subnet_lb_private_cidr" {
-  type        = string
-  description = "cidr block of subnet"
-  default     = "10.0.2.0/24"
-}
-
 # Object storage
 variable "soa_bucket_name" {
 }
 
-# DBSystem 
+# DBSystem
+variable "soa_db_system_display_name" {
+  default = "soa-db"
+}
+
 variable "db_system_shape" {
   default = "VM.Standard2.1"
 }
@@ -99,9 +97,16 @@ variable "db_system_edition" {
   default = "ENTERPRISE_EDITION"
 }
 
-variable "soa_db_system_display_name" {
+#Initial data storage size (in GB) must be one of [256, 512, 1024, 2048, 4096, 6144, 8192, 10240, 12288, 14336, 16384, 18432, 20480, 22528, 24576, 26624, 28672, 30720, 32768, 34816, 36864, 38912, 40960]
+variable "data_storage_size_in_gb" {
+  default = "256"
 }
 
+variable "db_hostname" {
+  default = "soadb"
+}
+
+# DBSystem and ATP common variables
 variable "db_version" {
   default = "18.8.0.0"
 }
@@ -110,7 +115,8 @@ variable "db_admin_password" {
   default = "BEstrO0ng_#12"
 }
 
-variable "soa_db1_name" {
+variable "db_name" {
+  default = "soadb"
 }
 
 variable "db_workload" {
@@ -121,24 +127,16 @@ variable "license_model" {
   default = "LICENSE_INCLUDED"
 }
 
-#Initial data storage size (in GB) must be one of [256, 512, 1024, 2048, 4096, 6144, 8192, 10240, 12288, 14336, 16384, 18432, 20480, 22528, 24576, 26624, 28672, 30720, 32768, 34816, 36864, 38912, 40960]
-variable "data_storage_size_in_gb" {
-  default = "256"
-}
-
-variable "soa_db_hostname" {
-}
-
 # Bastion
 variable "imageocids" {
   type = map(string)
 
   default = {
     # https://docs.cloud.oracle.com/iaas/images/
-    # E.g. CentOS-7-2020.02.22-0
-    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaahkaj2rzfdpruxajpy77gohgczstwhygsimohss2plkfslbbh4xfa"
+    # E.g. CentOS-7-2020.07.20-0
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaahz3knqno3jjbbatwyoakjr2csk27lkyuodi3m6eb4oogeaysidja"
 
-    eu-amsterdam-1 = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaat32fvq5hsmbljrvy77gr2xel7i3l3oc6g3bcnnd6mimzz5jqa7ka"
+    eu-amsterdam-1 = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaauyzgc25ex2qvgccqwqcyuzw6mbwdbls5c676hvcre2c54zdboviq"
   }
 }
 
